a68d3af29d9d2808ea81d5597be076af52830032
Greyson Kirk <greysonkirk@yahoo.com>
2020-11-10 21:06:59 -0600

    added comments to the code

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
+//setting api key and other variables that will be used
 let apiKey = '9f75af2cc9785ecfc80929ccd3080e4e'
-
 let cityName = 'Dallas'
 let curName = "";
 let curTemp = "";
@@ -9,13 +9,15 @@ let curUV = "";
 let curIcon = "";
 let cities = [];
 
+
+//these functions run on load.
 cityWeather();
 init();
 fiveDay();
 
 
 
-
+//hits the openweather api with Dallas as the default and runs after a search is done
 function cityWeather() {
     let today = moment().format("MM/D/YY");
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
@@ -23,11 +25,8 @@ function cityWeather() {
         url: queryURL
     }).then(response => {
 
-
+        //this call gets the uv index
         $.ajax(`http://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${apiKey}`).then(uvResp => {
-
-
-
             let uvData = `<p>UV Index: ${uvResp.value} </p>`
             $("#mainData").append(uvData)
         })
@@ -35,24 +34,27 @@ function cityWeather() {
         curTemp = response.main.temp;
         curWind = response.wind.speed;
         curHumid = response.main.humidity;
+        //setting weather icon for the img src link below
         curIcon = response.weather[0].icon;
         let mainData = `<h2>${curName} (${today})<img src="http://openweathermap.org/img/w/${curIcon}.png"></img></h2>
                             <p>Temperature: ${curTemp} °F</p>
                             <p>Humidity: ${curHumid}%</p>
                             <p>Wind Speed: ${curWind} MPH</p>
                            `
+            //appending the main city data                   
         $("#mainData").append(mainData)
 
     });
 };
 
-
+//getting the five day forecast
 function fiveDay() {
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=0&units=imperial&appid=${apiKey}`
     $.ajax({
         url: queryURL
     }).then(forecast => {
         console.log(forecast)
+            //skipping every 8 to only get the 5 days back and setting HTML for each day
         for (let i = 0; i < 40; i += 8) {
             foreDate = (forecast.list[i].dt_txt).slice(0, -8)
             foreTemp = forecast.list[i].main.temp
@@ -66,17 +68,13 @@ function fiveDay() {
                                <p class="card-text">Humidity: ${foreHumid}%</p>
                               </div>
                             </div>`
-
+                //appending each card built in the 5 day 
             $("#forecast").append(cardData)
         }
-
-
-
     });
-
 }
 
-
+//when search button is clicked, the main data and forecast are emptied out to prevent multiples 
 $("#search").on("click", function(event) {
     event.preventDefault();
     cityName = $("#city").val();
@@ -87,21 +85,19 @@ $("#search").on("click", function(event) {
     $("#mainData").empty();
     cityWeather(cityName);
     fiveDay(cityName);
+    //setting local storage to save users search history.
     localStorage.setItem("cities", JSON.stringify(cities));
 
 });
-
+//building the list of searches users made
 function buildList() {
-    console.log(cities)
-        //clear li elements so theres no repeats
+    //clear li elements so theres no repeats
     $("#history").empty();
     for (let i = 0; i < cities.length; i++) {
-        const city = cities[i];
-
+        const city = cities[i]
         let cityList = `<li id="list" class="list-group-item list-group-item-action">${city}</li>`
-
-        $("#history").append(cityList)
-
+            //prepend so the most recent is at the top
+        $("#history").prepend(cityList)
     }
 };
 
@@ -116,6 +112,7 @@ function init() {
     // Render search history
     buildList();
 }
+//so user can pull a city they have already searched for without typing.
 $("li").on('click', function(event) {
     event.preventDefault();
     let histSearch = $(this).closest("#list").text()
